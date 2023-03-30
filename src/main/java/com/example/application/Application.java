01659c43109f21e113373840b42743211f821744
Artur Signell <artur@vaadin.com>
2023-03-30 12:49:01 +0300

    Use an embedded Jetty

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,23 +1,43 @@
 package com.example.application;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
-@SpringBootApplication
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.theme.Theme;
+
 @Theme(value = "embedded-jetty")
 public class Application implements AppShellConfigurator {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws Exception {
+        final WebAppContext context = createWebAppContext();
+        Server server = new Server(9090);
+        server.setHandler(context);
+        server.start();
+        server.join();
+
+    }
+
+    // copied from: https://github.com/mvysny/vaadin-boot/tree/main/vaadin-boot
+    private static WebAppContext createWebAppContext() throws IOException {
+        final WebAppContext context = new WebAppContext();
+        context.setBaseResource(Resource.newResource("src/main/webapp"));
+        context.addServlet(VaadinServlet.class, "/*");
+        // this will properly scan the classpath for all @WebListeners,
+        // including the most important
+        // com.vaadin.flow.server.startup.ServletContextListeners.
+        // See also https://mvysny.github.io/vaadin-lookup-vs-instantiator/
+        context.setAttribute(
+                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*\\.jar|.*/classes/.*");
+        context.setConfigurationDiscovered(true);
+        context.getServletContext().setExtendedListenerTypes(true);
+
+        return context;
     }
 
 }
